5aba64370805046b05b42d9140efba5ace5f6946
Ridhwankiki <ridhwanr0607@gmail.com>
2024-04-17 13:08:03 +0700

    Initial Commit

---
+++ CoffeShop/CoffeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeShopApp.swift
+//  CoffeShop
+//
+//  Created by MacBook Pro on 17/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
